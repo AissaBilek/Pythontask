482a57f2bd8ddc0301f759535e4c8eb606a30169
Bilek Aissa <aissayacineblk@gmail.com>
2023-08-01 14:53:35 +0100

    creation of a new method to extract data

--- Beautifulsoup.py
+++ Beautifulsoup.py
@@ -2,19 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 class Beautifuloupclasse:
-
+#a constructor method of the class
     def _init_(self,link):
         self.link= link
-
+#adding a new method to extract data from the website
+    def extractData(self):
+        services_providing=requests.get(self.link)
+        print(services_providing)
+        html = BeautifulSoup(services_providing.content, 'html.parser')
+        infraXcode_services=html.select(".elementor-widget-wrap.elementor-element-populated h2 a")
+        return infraXcode_services
     
 
 
-html_page = requests.get('https://www.infraxcode.com/')
 
-services_providing=requests.get('https://www.infraxcode.com/fournisseur-de-solutions-dautomatisation/')
-print(services_providing)
-
-html = BeautifulSoup(services_providing.content, 'html.parser')
 
 infraXcode_services=html.select(".elementor-widget-wrap.elementor-element-populated h2 a")
 for item in infraXcode_services:
