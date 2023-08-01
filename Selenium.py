eda3a1d1a56a4892228d82ff5198acacf71c3ed5
Bilek Aissa <aissayacineblk@gmail.com>
2023-08-01 12:03:17 +0100

    new commit

--- Selenium.py
+++ Selenium.py
@@ -1,15 +1,15 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-#configuration te3 webdriver
+#webdriver configuration
 from selenium.webdriver.chrome.options import Options
 
 
 # Create a Chrome WebDriver instance
 options = Options()
 options.add_argument("--headless")
-#creature d'instance "options" on l'utilise pour des différents options pour google chrome
 
+#options instance
 driver = webdriver.Chrome(options=options)
 
 
@@ -22,7 +22,7 @@ service_class_name = ".elementor-widget-wrap.elementor-element-populated"
 # css selector
 css_selector = f"{service_class_name} h2 a"
 
-# Find an element by its XPath and extract its text content
+# Find an element by its css selector and extract its text content
 elements = driver.find_elements(By.CSS_SELECTOR, css_selector)
 for element in elements:
     print(element.text)
