bff2762d285c868b366e9915d7d62a95cbae55a9
Bilek Aissa <aissayacineblk@gmail.com>
2023-08-02 11:23:44 +0100

    importing the second classe on the main

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ from Beautifulsoup import Beautifuloupclasse
 link='https://www.infraxcode.com/fournisseur-de-solutions-dautomatisation/'
 b=Beautifuloupclasse(link)
 b.createExcelSheet()
+#the 2st library file
+from Selenium import Selenuimclass
+s=Selenuimclass(link)
+s.createExcelSheet()
